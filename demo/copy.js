31339c60b429baf63e72e57107d0530ec4febe8f
Paige Gorry <paigeegorry@gmail.com>
2019-01-15 11:05:37 -0800

    testing with promises!

--- demo/copy.js
+++ demo/copy.js
@@ -1,6 +1,17 @@
-const fsPromises = require('fs').promises;
+const fsPromise = require('fs').promises;
 
-fsPromises.readFile('./promises.md', { encoding: 'utf8' })
-  .then(data => fsPromises.writeFile('./promises-copy.md', data))
-  .then(() => console.log('done'))
-  .catch(err => console.error(err));
+module.exports = (src, dst) => {
+  return fsPromise.readFile(src, { encoding: 'utf8' })
+    .then(data => fsPromise.writeFile(dst, data))
+    .then(() => console.log('DONE!'))
+    .catch(err => console.error(err));
+};
+
+// function copy(src, dst) {
+//   fsPromise.readFile('./promises.md', { encoding: 'utf8' })
+//     .then(data => fsPromise.writeFile('./promises-copy.md', data))
+//     .then(() => console.log('done'))
+//     .catch(err => console.error(err));
+
+// }
+
