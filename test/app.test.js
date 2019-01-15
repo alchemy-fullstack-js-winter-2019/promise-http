5d616041150b9f22af22803c97abb650176ccb02
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-15 15:32:24 -0800

    json app and test, passes!

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,18 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app', () => {
+  // it('has a testing route', () => {
+  //   return request(app)
+  //     .get('/tester')
+  //     .then(res => {
+  //       expect(res.text).toEqual('testing123');
+  //     });
+  // });
   it('has a testing route', () => {
     return request(app)
       .get('/tester')
       .then(res => {
-        expect(res.text).toEqual('testing123');
+        expect(res.body).toEqual({ testing: 123 });
       });
   });
 });
