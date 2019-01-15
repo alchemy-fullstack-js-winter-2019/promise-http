bfa62d01889fd24fec8490e0638fd13b2cdc21bd
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-15 10:09:26 -0800

    read file

--- promises.js
+++ promises.js
@@ -1,6 +1,8 @@
 const fsPromises = require('fs').promises;
 
-fsPromises.readFile()
+fsPromises.readFile(file);
+  console.log(file);
+  
 promise
   .then(date => console.log(data));
   .catch(err => console.log(err));\
