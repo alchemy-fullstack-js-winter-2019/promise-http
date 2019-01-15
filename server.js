068a2fbace6ff9f1e81358adabddcb5414053ae5
Paige Gorry <paigeegorry@gmail.com>
2019-01-15 14:53:04 -0800

    respond based on path

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const http = require('http');
+const { parse } = require('url');
 
 http.createServer((req, res) => {
   console.log('request incoming');
+  const url = parse(req.url);
   res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><p>Thanks for visiting!</p></body></html>');
+  if(url.path === '/birthday') {
+    res.end(`<html>
+      <body>
+        <p>Happy Birthday!</p>
+      </body>
+    </html>`);
+  }
+  else if(url.path === '/tomorrow') {
+    res.end(`<html>
+      <body>
+        <p>Tomorrow, tomorrow!</p>
+      </body>
+    </html>`);
+  }
+  else if(url.path === '/birthday/tomorrow') {
+    res.end(`<html>
+      <body>
+        <p>Your birthday is tomorrow!</p>
+      </body>
+    </html>`);
+  }
 })
   .listen(7890);
