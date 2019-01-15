b3275026f974bfddb5f0a34b04bcf042ad9c4472
Paige Gorry <paigeegorry@gmail.com>
2019-01-15 14:23:33 -0800

    all tests passing, including the transformer demo

--- demo/transformer.test.js
+++ demo/transformer.test.js
@@ -2,7 +2,7 @@ const transformer = require('./transformer');
 
 describe('transformer', () => {
   it('transforms some file', () => {
-    return transformer('./transform.txt')
+    return transformer('./demo/transform.txt')
       .then(transTxt => {
         expect(transTxt).toEqual('EREH I');
       });
