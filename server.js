94871e3ed3abe2b12cd08199963fa0618c687e39
Paige Gorry <paigeegorry@gmail.com>
2019-01-15 15:08:33 -0800

    server and app refactoring

--- server.js
+++ server.js
@@ -1,30 +1,5 @@
 const http = require('http');
-const { parse } = require('url');
+const app = require('./lib/app');
 
-http.createServer((req, res) => {
-  console.log('request incoming');
-  const url = parse(req.url);
-  res.setHeader('Content-Type', 'text/html');
-  if(url.path === '/birthday') {
-    res.end(`<html>
-      <body>
-        <p>Happy Birthday!</p>
-      </body>
-    </html>`);
-  }
-  else if(url.path === '/tomorrow') {
-    res.end(`<html>
-      <body>
-        <p>Tomorrow, tomorrow!</p>
-      </body>
-    </html>`);
-  }
-  else if(url.path === '/birthday/tomorrow') {
-    res.end(`<html>
-      <body>
-        <p>Your birthday is tomorrow!</p>
-      </body>
-    </html>`);
-  }
-})
+http.createServer(app)
   .listen(7890);
