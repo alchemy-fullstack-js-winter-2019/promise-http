55fa0d063a4a06818df398b212df89b3382c653b
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-15 15:00:21 -0800

    html changes based on path name; git statu

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const http = require('http');
+const { parse } = require('url');
 
 http.createServer((req, res)  => {
-    console.log('request incoming');
-    var body = '<html><body>Thanks for visiting!</body></html>';
-
-    res.setHeader('Content-Type', 'text/html');
-    res.end(body);
-
+    const url  = parse(req.url);
+    console.log(url);
+    
+    if(url.pathname === '/birthday') {
+        var bday = '<html><body>Happy Birthday boiiii</body></html>';
+        res.setHeader('Content-Type', 'text/html');
+        res.end(bday);
+    }
+    else if(url.pathname === '/tomorrow') {
+        var tomorrow = '<html><body> Tomorrow, Tomorrow </body></html>';
+        res.setHeader('Content-Type', 'text/html');
+        res.end(tomorrow);
+    }
+    
+    else {
+        console.log('request incoming');
+        var body = '<html><body>Thanks for visiting!</body></html>';
+        res.setHeader('Content-Type', 'text/html');
+        res.end(body);
+    }
+    
 
 
 }).listen(3000);
