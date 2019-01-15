8d92c6acad276ffa94bb0938b6195fb98c5c3659
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-15 13:51:51 -0800

    Promise.all

--- api.js
+++ api.js
@@ -1,7 +1,18 @@
+/* eslint-disable no-console */
 const request = require('superagent');
 
 request
   .get('https://rickandmortyapi.com/api/character/')
   .then(res => {
-    console.log(res.body);
-  });
+    return res.body.results
+      .map(character => character.origin.url)
+      .filter(originUrl => originUrl !== '');
+  })
+  .then(originUrls => {
+    return Promise.all(originUrls.map(url => {
+      return request.get(url);
+    }));
+    
+  })
+  .then(originRess => originRess.map(originRes => originRes.body)) // get body only
+  .then(origins => console.log(origins));
