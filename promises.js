10fed01e47e59371d30d83e2ee3bc2bf1046ecde
Kate Dameron <katerj33@gmail.com>
2019-01-15 12:21:07 -0800

    transformer passing all tests

--- promises.js
+++ promises.js
@@ -1,26 +1,26 @@
 /*eslint-disable no-console*/
-const fsPromises = require('fs').promises;
+const fsPromise = require('fs').promises;
 
 // pending -> waiting for the promise to finish
 // fulfilled -> the promise finished and resolved
 // rejected -> the promise finished and rejected
-fsPromises.readFile('./http.md', { encoding: 'utf8' })
+fsPromise.readFile('./http.md', { encoding: 'utf8' })
   // then is called once promise is fulfilled
   .then(data => console.log(data))
   // catches the error if rejected
   .catch(err => console.error(err));
 
-fsPromises.writeFile('test.txt', 'testing 123')
+fsPromise.writeFile('test.txt', 'testing 123')
   .then(() => console.log('Done'))
   .catch(err => console.error(err));
 
 function copyFile(src, dst) {
-  fsPromises.readFile(src, { encoding: 'utf8' })
-    .then(data => fsPromises.writeFile(dst, data)
+  fsPromise.readFile(src, { encoding: 'utf8' })
+    .then(data => fsPromise.writeFile(dst, data)
       .then(() => console.log('Done'))
       .catch(err => console.error(err))
     );  
 }
 
-module.exports = copyFile;
+module.exports = { copyFile };
 
--- promises.test.js
+++ promises.test.js
@@ -1,18 +1,27 @@
-const { copyFile } = require('./promises');
-const fs = require('fs');
+/*eslint-disable no-console*/
+const copyFile  = require('./copy');
+const fsPromise = require('fs').promises;
 
 describe('copy', () => {
+  afterEach(() => {
+    return fsPromise.unlink('./http-copy.md');
+  });
+
   it('copies a file', () => {
     // Return Promise
     return copyFile('./http.md', './http-copy.md')
       .then(() => {
-        // Then use all method on Promise class && pass readFile to array
-        Promise.all([fs.Promises.readFile('./http.md'), fs.Promises.readFile('./http-copy.md')])
+        // Then use .all method on Promise class && pass readFile to array
+        Promise.all([
+          fsPromise.readFile('./http.md'), 
+          fsPromise.readFile('./http-copy.md')
+        ])
           // Then destructure the files
           .then(([httpMd, httpCopyMd]) => {
             // Then expect them to be equal
             expect(httpMd).toEqual(httpCopyMd);
-          });
+          })
+          .catch(err => expect(err).toBeFalsy());
       });
   });
 });
