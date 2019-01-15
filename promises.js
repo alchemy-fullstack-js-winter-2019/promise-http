1fd293e347b32c4fb0aa8a970bc5a9bbc30a31d1
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-15 10:19:23 -0800

    writeFile

--- promises.js
+++ promises.js
@@ -2,7 +2,14 @@ const fsPromises = require('fs').promises;
 
 fsPromises.readFile(file);
   console.log(file);
-  
-promise
-  .then(date => console.log(data));
-  .catch(err => console.log(err));\
+
+  // pending -> waiting for the promise to finish
+  // fulfilled -> the promise finished and resolved
+  // rejected -> the promise finished and rejected
+fsPromises.readFile('./http.md', { encoding: 'utf8' })
+  .then(data => console.log(data));
+  .catch(err => console.log(err));
+
+fsPromises.writeFile('test.txt', 'testing123', { encoding: 'utf8' })
+  .then(() => console.log('DONE'));
+  .catch(err => console.error('ERROR'));
