76e445267e11ccb07f3106bd0b09a51e77b235c2
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-15 15:03:56 -0800

    server responds based on path

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 /* eslint-disable no-console */
 const http = require('http');
+const { parse } = require('url');
 
 http.createServer((req, res) => {
-  console.log('request incoming');
-  res.end('hi there');
+  const url = parse(req.url);
+ 
+  if(url.pathname === '/birthday') {
+    res.end('Happy birthday!');
+  } else if(url.pathname === '/tomorrow') {
+    res.end('Tomorrow, tomorrow');
+  } else if(url.pathname === '/birthday/tomorrow') {
+    res.end('Tomorrow is your birthday');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+  
+
+  res.setHeader({ 'Content-Type': 'text/html' });
+  res.end(`
+  <html>
+    <body>
+      <p>Thanks for visiting!</p>
+    </body>
+  </html>`);
 })
   .listen(7890);
 
