7db266dc3160fef647fd7bd1431ec1550ddb4474
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-15 10:33:33 -0800

    copy http.md file

--- promises.js
+++ promises.js
@@ -1,15 +1,18 @@
 const fsPromises = require('fs').promises;
 
-fsPromises.readFile(file);
-  console.log(file);
 
-  // pending -> waiting for the promise to finish
-  // fulfilled -> the promise finished and resolved
-  // rejected -> the promise finished and rejected
+// pending -> waiting for the promise to finish
+// fulfilled -> the promise finished and resolved
+// rejected -> the promise finished and rejected
 fsPromises.readFile('./http.md', { encoding: 'utf8' })
-  .then(data => console.log(data));
+  .then(data => console.log(data))
   .catch(err => console.log(err));
 
-fsPromises.writeFile('test.txt', 'testing123', { encoding: 'utf8' })
-  .then(() => console.log('DONE'));
-  .catch(err => console.error('ERROR'));
+fsPromises.writeFile('test.txt', 'Data to write')
+  .then(() => console.log('DONE'))
+  .catch(err => console.error(err));
+
+fsPromises.readFile('./http.md', { encoding: 'utf8' })
+  .then(data => fsPromises.writeFile('./http-copy.md', data))
+  .then(() => console.log('DONE'))
+  .catch(err => console.log(err));
