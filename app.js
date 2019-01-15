d9ebe7817d1191d98cbe9c28361bed0921ca02cd
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-15 15:04:01 -0800

    serperated app/server

---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const { parse } = require('url');
+
+const server = http.createServer((req, res)  => {
+    const url  = parse(req.url);
+    console.log(url);
+    
+    if(url.pathname === '/birthday') {
+        var bday = '<html><body>Happy Birthday boiiii</body></html>';
+        res.setHeader('Content-Type', 'text/html');
+        res.end(bday);
+    }
+    else if(url.pathname === '/tomorrow') {
+        var tomorrow = '<html><body> Tomorrow, Tomorrow </body></html>';
+        res.setHeader('Content-Type', 'text/html');
+        res.end(tomorrow);
+    }
+    
+    else {
+        console.log('request incoming');
+        var body = '<html><body>Thanks for visiting!</body></html>';
+        res.setHeader('Content-Type', 'text/html');
+        res.end(body);
+    }
+    
+
+
+})
+
+module.exports = server;
--- server.js
+++ server.js
@@ -1,30 +1,36 @@
-const http = require('http');
-const { parse } = require('url');
+// const http = require('http');
+// const { parse } = require('url');
 
-http.createServer((req, res)  => {
-    const url  = parse(req.url);
-    console.log(url);
+// http.createServer((req, res)  => {
+//     const url  = parse(req.url);
+//     console.log(url);
     
-    if(url.pathname === '/birthday') {
-        var bday = '<html><body>Happy Birthday boiiii</body></html>';
-        res.setHeader('Content-Type', 'text/html');
-        res.end(bday);
-    }
-    else if(url.pathname === '/tomorrow') {
-        var tomorrow = '<html><body> Tomorrow, Tomorrow </body></html>';
-        res.setHeader('Content-Type', 'text/html');
-        res.end(tomorrow);
-    }
+//     if(url.pathname === '/birthday') {
+//         var bday = '<html><body>Happy Birthday boiiii</body></html>';
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end(bday);
+//     }
+//     else if(url.pathname === '/tomorrow') {
+//         var tomorrow = '<html><body> Tomorrow, Tomorrow </body></html>';
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end(tomorrow);
+//     }
     
-    else {
-        console.log('request incoming');
-        var body = '<html><body>Thanks for visiting!</body></html>';
-        res.setHeader('Content-Type', 'text/html');
-        res.end(body);
-    }
+//     else {
+//         console.log('request incoming');
+//         var body = '<html><body>Thanks for visiting!</body></html>';
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end(body);
+//     }
     
 
 
-}).listen(3000);
+// })
+
+const server = require('./app.js');
+
+server.listen(3000, () => {
+    console.log('Sever listening on Port 3000');
+});
 
 
