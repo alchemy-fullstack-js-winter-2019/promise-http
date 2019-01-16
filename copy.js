1aa274dc4891e7efa5dba52153501e1415d1a6fa
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-15 17:49:33 -0800

    trying to fix travis

--- copy.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-console */
-const fsPromises = require('fs').promises;
-module.exports = function copy(src, dst) {
-  return fsPromises.readFile(src)
-    .then(data => fsPromises.writeFile(dst, data))
-    .catch((err) => console.log(err));
-};
