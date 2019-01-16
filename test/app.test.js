199f04f94d26d6c29e3270eee173c304f8e83f1b
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-15 17:23:36 -0800

    query url search working

--- test/app.test.js
+++ test/app.test.js
@@ -2,14 +2,30 @@ const request  = require('supertest');
 const app = require('../lib/app');
 
 describe('test app', () => {
-    it('has a testin route', () => {
+    it('has a testing route', () => {
         return request(app)
             .get('/tester')
             .then(res => {
-                expect(res.text).toEqual({ testing: 123 });
+                expect(res.body).toEqual({ testing: 123 });
             });
-
     });
+    it('can response based on a query string', () => {
+        return request(app) 
+            .get('/you?name=lance')
+            // .query({ name: 'lance' })
+            .then(res => {
+                expect(res.body).toEqual({ hi: 'there lance' });
+            });
+    });
+    it('it can post data', () => {
+        return request(app)
+            .post('./notes')
+            .send({ text: 'Im a note' })
+            .then(res => {
+                expect(res.status).toEqual(204);
+            });
+    });
+
 });
 
 
