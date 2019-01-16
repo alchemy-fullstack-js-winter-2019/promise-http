c23d9c2a5eee6ad4d78ad7509b8be225ab9c65e1
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-15 17:22:50 -0800

    bodyParser tests work, RandM tests do not

---
+++ test/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('app', () => {
+  it('has a tester route', () => {
+    return request(app)
+      .post('/note')
+      .send({ text: 'Im a note' })
+      .then(res => {
+        expect(res.status).toEqual(204);
+      });
+  });
+});
