63580e8c11999b06acf2c0dbe013ad88408044b7
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-16 08:31:40 -0800

    deleted test function because travis didnt like it

--- morning/promises.js
+++ morning/promises.js
@@ -15,23 +15,4 @@ fsPromises.readFile('./promises.md', { encoding: 'utf8' })
     .catch(err => console.error(err));
     
 
-const readPromise = src => new Promise((resolve, reject) => { 
-    
-    fs.readFile(src, { encoding: 'uft8' }, (err, data) => {  
-        if(err) return reject(err);
-        resolve(data);
-    });
-});
 
-
-
-readPromise('./transform')
-    .then((data) => console.log(data));
-
-
-
-
-
-
-
-       
