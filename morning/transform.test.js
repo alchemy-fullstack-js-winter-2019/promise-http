4fee81606cb51e003711a5be1560e94d09307c48
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-16 08:56:45 -0800

    changed file path for travis again

--- morning/transform.test.js
+++ morning/transform.test.js
@@ -4,7 +4,7 @@ const transformer = require('./transform');
 
 describe('does things', () => {
     it('transforms a file', () => {
-        return transformer('./transform.txt')
+        return transformer('./morning/doThings.txt')
             .then(transTxt => {
                 expect(transTxt).toEqual('POOUU');
             });
