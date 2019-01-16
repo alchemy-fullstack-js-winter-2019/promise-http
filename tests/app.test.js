3e03f32d8224889aa592279ef9dfe2b3be7ab3da
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-16 15:50:57 -0800

    writing post test

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,6 +4,15 @@ const app = require('../lib/app');
 jest.mock('../service/RickAndMortyApi.js');
 
 describe('app', () => {
+
+  it('can take a query string', () => {
+    return request(app)
+      .get('/you?name=unclebob')
+      .then(res => {
+        expect(res.body).toEqual({ test: 'hi there unclebob' });
+      });
+  });
+
   it('gets a character by id', () => {
     return request(app)
       .get('/character/1')
@@ -15,5 +24,27 @@ describe('app', () => {
         });
       });
 
+  });
+
+  it('can return all characters', () => {
+    return request(app)
+      .get('/character')
+      .then(res => {
+        expect(res.text).toEqual(`<html>
+        <body>
+
+        </body>
+        </html>
+        `);
+      });
+  });
+
+  it('can post a note to a specific character', () => {
+    return request(app)
+      .post('/character')
+      .send({ characterId: 1, note: 'Its pickle Rick' })
+      .then(res => {
+        expect(res.status).toEqual(204)
+      });
   });
 });
