d4fa87685d38e1767c474f23ace6ae3a9de1ebda
JeiWolfei <jvampy22@gmail.com>
2019-01-15 17:28:46 -0800

    write get and post functions into app

--- app.js
+++
@@ -1,8 +0,0 @@
- const { parse } = require('url');
-
-module.exports = (req, res) => {
-  const url = parse(req.url);
-  if(url.pathname === '/tester') {
-    res.end('testing123');
-  }
-};
--- test/app.test.js
+++ test/app.test.js
@@ -4,9 +4,14 @@ const app = require('../lib/app');
 describe('app', () => {
   it('has a tester route', () => {
     return request(app)
-      .get('/tester')
+      // .get('/tester')
+      // .query({ name: 'ryan ' })
+      // .then(res => {
+      .post('/note')
+      .send({ 'im a note' })
       .then(res => {
-        expect(res.text).toEqual('testing123');
+        expect(res.text).toEqual(204);
       });
   });
 });
+
