133f45aa4e63038c013ba40f984b1af0087e82ce
Kate Dameron <katerj33@gmail.com>
2019-01-16 17:28:44 -0800

    last 'get' in progress

--- test/app.test.js
+++ test/app.test.js
@@ -6,28 +6,31 @@ jest.mock('../lib/service/rickAndMortyApi.js');
 
 describe('app', () => {
   it('writes a list of characters to browser', () => {
+    // Using supertest to make a fake server to run app which is an http listener
     return request(app)
       .get('/characters')
       .then(res => {
-        console.log('test', res.text);
         expect(res.text).toEqual(
           `<html><body><ul><li>Name: Rick Sanchez, Status: Alive, Species: Human</li>,<li>Name: Morty Smith, Status: Alive, Species: Human</li></ul></body></html>`
         );
       });
   });
+
+  it('save a note to a note object', () => {
+    return request(app)
+      .post('/characters')
+      .send({ characterId: 1234, note: 'I\'m a note' })
+      .then(res => {
+        expect(res.status).toEqual(204);
+      });
+  });
+
+  it('returns a character with all notes', () => {
+    return request(app)
+      .get('/characters/1234')
+      .then(res => {
+        expect().toEqual();
+      });
+  });
+
 });
-
-
-
-
-
-// describe('app', () => {
-//   it('has a tester route', () => {
-//     return request(app)
-//       .post('/note')
-//       .send({ text: 'Im a note' })
-//       .then(res => {
-//         expect(res.status).toEqual(204);
-//       });
-//   });
-// });
