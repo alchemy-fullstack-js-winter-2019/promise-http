d1cdc95bfea5d8b980aaef39982f8d6422c47afc
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-17 06:53:54 -0800

    refactored to use 2 module.exports: app and test

--- test/app.test.js
+++ test/app.test.js
@@ -2,13 +2,13 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 jest.mock('../lib/services/rickAndMortyApi.js', () => ({
-  // getCharacter() {
-  //   return Promise.resolve({
-  //     name: 'Rick Sanchez',
-  //     species: 'Human',
-  //     status: 'Alive'
-  //   });
-  // },
+  getCharacter() {
+    return Promise.resolve({
+      name: 'Rick Sanchez',
+      species: 'Human',
+      status: 'Alive'
+    });
+  },
   getCharacters() {
     return Promise.resolve([
       {
@@ -26,120 +26,100 @@ jest.mock('../lib/services/rickAndMortyApi.js', () => ({
   }
 }));
 
-// jest.mock('../lib/services/rickAndMortyApi.js', () => ({
-//   getCharacters() {
-//     return Promise.resolve(['Rick Sanchez',
-//       'Morty Smith',
-//       'Summer Smith',
-//       'Beth Smith',
-//       'Jerry Smith',
-//       'Abadango Cluster Princess',
-//       'Abradolf Lincler',
-//       'Adjudicator Rick',
-//       'Agency Director',
-//       'Alan Rails',
-//       'Albert Einstein',
-//       'Alexander',
-//       'Alien Googah',
-//       'Alien Morty',
-//       'Alien Rick',
-//       'Amish Cyborg',
-//       'Annie',
-//       'Antenna Morty',
-//       'Antenna Rick',
-//       'Ants in my Eyes Johnson']);
-//   }
-// }));
-
 describe('app', () => {
-  // server and app
-  // it('has a testing route', () => {
-  //   return request(app)
-  //     .get('/tester')
-  //     .then(res => {
-  //       expect(res.text).toEqual('testing123');
-  //     });
-  // });
-
-  // json app
-  // it('has a testing route', () => {
-  //   return request(app)
-  //     .get('/tester')
-  //     .then(res => {
-  //       expect(res.body).toEqual({ testing: 123 });
-  //     });
-  // });
-
-  // Query Strings
-  // it('has a testing route', () => {
-  //   return request(app)
-  //     .get('/you')
-  //     .query({ name: 'cari' })
-  //     .then(res => {
-  //       expect(res.body).toEqual({ hi: 'there cari' });
-  //     });
-  // });
-
-  // POSTing data
-  // it('has a POST route', () => {
-  //   return request(app)
-  //     .post('/note')
-  //     .send({ text: 'Im a note' }) 
-  //     .then(res => {
-  //       expect(res.status).toEqual(204);
-  //     });
-  // });
-
-  // rick and morty character by ID
-  // it('can return JSON that displays character details by ID', () => {
-  //   return request(app)
-  //     .get('/character/1')
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         name: 'Rick Sanchez',
-  //         species: 'Human',
-  //         status: 'Alive'
+  // describe('top module.exports chunk', () => {
+  //   // server and app
+  //   it('has a server and app testing route', () => {
+  //     return request(app)
+  //       .get('/tester123')
+  //       .then(res => {
+  //         expect(res.text).toEqual('testing123');
   //       });
-  //     });
-  // });
-
-  // Rick and Morty notes
-  // it('can return HTML that displays all characters on pg1', () => {
-  //   return request(app)
-  //     .get('/characters/')
-  //     .then(res => {
-  //       expect(res.text).toString(`
-  //       <html>
-  //         <body>
-  //           <li>name: Rick Sanchez, status: Alive, species: Human</li>
-  //           <li>name: Morty Smith, status: Alive, species: Human</li>
-  //         </body>
-  //       </html>
-  //       `);
-  //     });
+  //   });
+  
+  //   // json app
+  //   it('has a json app testing route', () => {
+  //     return request(app)
+  //       .get('/tester')
+  //       .then(res => {
+  //         expect(res.body).toEqual({ testing: 123 });
+  //       });
+  //   });
+  
+  //   // Query Strings
+  //   it('has a query strings testing route', () => {
+  //     return request(app)
+  //       .get('/you')
+  //       .query({ name: 'cari' })
+  //       .then(res => {
+  //         expect(res.body).toEqual({ hi: 'there cari' });
+  //       });
+  //   });
+  
+  //   // POSTing data
+  //   it('has a POST route', () => {
+  //     return request(app)
+  //       .post('/note')
+  //       .send({ text: 'Im a note' }) 
+  //       .then(res => {
+  //         expect(res.status).toEqual(204);
+  //       });
+  //   });
+  
+  //   // rick and morty character by ID
+  //   it('can return JSON that displays character details by ID', () => {
+  //     return request(app)
+  //       .get('/character/1')
+  //       .then(res => {
+  //         expect(res.body).toEqual({
+  //           name: 'Rick Sanchez',
+  //           species: 'Human',
+  //           status: 'Alive'
+  //         });
+  //       });
+  //   });
+  
+  //   // Rick and Morty notes
+  //   it('can return HTML that displays all characters on pg1', () => {
+  //     return request(app)
+  //       .get('/characters/')
+  //       .then(res => {
+  //         expect(res.text).toString(`
+  //         <html>
+  //           <body>
+  //             <li>name: Rick Sanchez, status: Alive, species: Human</li>
+  //             <li>name: Morty Smith, status: Alive, species: Human</li>
+  //           </body>
+  //         </html>
+  //         `);
+  //       });
+  //   });
   // });
 
   // POST to /characters
-  it('has a POST route', () => {
-    return request(app)
-      .post('/characters')
-      .send({ characterId: 1234, note: 'My favorite character' }) 
-      .then(res => {
-        expect(res.status).toEqual(204);
-      });
-  });
+  describe('bottom module.exports chunk', () => {
+    it('has a POST route', () => {
+      return request(app)
+        .post('/characters')
+        .send({ characterId: 1234, note: 'My favorite character' }) 
+        .then(res => {
+          expect(res.status).toEqual(204);
+        });
+    });
+  
+    // GET to /characters/1234 NOT YET WORKING
+    // beforeEach('posts a note', () => {
+    //   return request(app)
+    //     .post('/characters')
+    //     .send({ characterId: 1234, note: 'this is a note' });
+    // });
+    // it('returns HTML that displays a character and all notes about them', () => {
+    //   return request(app)
+    //     .get('/characters/1234')
+    //     .then(res => {
+    //       expect(res.text).toString();
+    //     });
+    // });
 
-  // GET to /characters/1234
-  // beforeEach('posts a note', () => {
-  //   return request(app)
-  //     .post('/characters')
-  //     .send({ characterId: 1234, note: 'this is a note' });
-  // });
-  // it('returns HTML that displays a character and all notes about them', () => {
-  //   return request(app)
-  //     .get('/characters/1234')
-  //     .then(res => {
-  //       expect(res.text).toString();
-  //     });
-  // });
+  });
 });
