c4464b8517beb86e9690f4093cc0ca4c1fb53f52
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-16 17:03:22 -0800

    passing the get notes for a character test

--- test/rickAndMortyNotesApp.test.js
+++ test/rickAndMortyNotesApp.test.js
@@ -4,6 +4,13 @@ const app = require('../lib/rickAndMortyNotesApp');
 jest.mock('../lib/services/rickAndMortyGetCharactersApi.js');
 
 describe('rick and morty notes app', () => {
+  beforeEach(() => {
+    return request(app)
+      .post('/characters')
+      .send({ characterId: 1, note: 'My favorite character' })
+      .send({ characterId: 2, note: 'FOOTBALL TEAMS!' });
+  });
+  
   it('can list the characters on the characters route', () => {
     return request(app)
       .get('/characters')
@@ -26,9 +33,26 @@ describe('rick and morty notes app', () => {
   it('can post notes', () => {
     return request(app)
       .post('/characters')
-      .send({ characterId: 1234, note: 'My favorite character' })
+      .send({ characterId: 1, note: 'My favorite character' })
       .then(res => {
         expect(res.statusCode).toEqual(200);
+      });
+  });
+
+  it('can display notes for characters', () => {
+    return request(app)
+      .get('/characters/2')
+      .then(res => {
+        expect(res.text).toEqual(
+      `
+        <html>
+          <body>
+            </ul>
+            <li>FOOTBALL TEAMS!</li><li>FOOTBALL TEAMS!</li><li>FOOTBALL TEAMS!</li>
+            </ul>
+          </body>
+        </html>`
+        );
       });
   });
 });
