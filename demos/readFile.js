b7b3b96219974cae13b39069b85a77d32435405b
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-20 09:26:46 -0800

    cleanup bodyParser, remove inclass demo files

--- demos/readFile.js
+++
@@ -1,5 +0,0 @@
-const fsPromises = require('fs').promises;
-
-fsPromises.readFile('./promises.md', { encoding: 'utf8' })
-  .then(data => console.log(data)) // eslint-disable-line no-console
-  .catch(err => console.log(err)); // eslint-disable-line no-console
--- demos/writeFile.js
+++
@@ -1,9 +0,0 @@
-const fsPromises = require('fs').promises;
-
-fsPromises.writeFile('./promises-copy.md', 'Writing stuff', { encoding: 'utf8' })
-  .then(data => data)
-  .catch(err => console.log(err)); // eslint-disable-line no-console
-//OR
-fsPromises.writeFile('./promises-copy.md', 'Writing MORE stuff')
-  .then(() => console.log('done!')) // eslint-disable-line no-console
-  .catch(err => console.log(err)); // eslint-disable-line no-console
