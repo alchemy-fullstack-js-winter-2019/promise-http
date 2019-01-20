bf5a5f820268697f25cfa57aeb87230dcf2871a3
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-20 13:42:19 -0800

    server and app can respond with text or json, and tests

--- test/app.test.js
+++ test/app.test.js
@@ -4,37 +4,13 @@ const app = require('../lib/app');
 jest.mock('../lib/services/rickAndMortyApi.js');
 
 describe('app', () => {
-  // TOP CHUNK
-  // describe('top module.exports chunk', () => {
-  //   // server and app
-  //   it('has a server and app testing route', () => {
-  //     return request(app)
-  //       .get('/tester123')
-  //       .then(res => {
-  //         expect(res.text).toEqual('testing123');
-  //       });
-  //   });
-  
-  //   // json app
-  //   it('has a json app testing route', () => {
-  //     return request(app)
-  //       .get('/tester')
-  //       .then(res => {
-  //         expect(res.body).toEqual({ testing: 123 });
-  //       });
-  //   });
-
-  // });
-
-
-  describe('bottom module.exports chunk', () => {
-    // Rick and Morty notes
-    // GET /characters return HTML that displays a list of characters
-    it('can return HTML that displays all characters on pg1', () => {
-      return request(app)
-        .get('/characters/')
-        .then(res => {
-          expect(res.text).toString(`
+  // Rick and Morty notes
+  // GET /characters return HTML that displays a list of characters
+  it('can return HTML that displays all characters on pg1', () => {
+    return request(app)
+      .get('/characters/')
+      .then(res => {
+        expect(res.text).toString(`
           <html>
             <body>
               <li>name: Rick Sanchez, status: Alive, species: Human</li>
@@ -42,81 +18,80 @@ describe('app', () => {
             </body>
           </html>
           `);
-        });
-    });
-    // POST /characters saves a note to the notes object
-    it('has a POST route and can save notes by characterId', done => {
-      return request(app)
-        .post('/characters')
-        .send({
-          characterId: 11, 
-          note: 'Rad character' 
-        })
-        .then(() => {
-          return request(app)
-            .post('/characters')
-            .send({
-              characterId: 11, 
-              note: 'another note about same character' 
-            });
-        })
-        .then(() => {
-          return request(app)
-            .post('/characters')
-            .send({
-              characterId: 12, 
-              note: 'Different character note' 
-            });
-        })
-        .then(res => {
-          expect(res.status).toEqual(204);
-          done();
-        });
-    });
-
-    // GET /characters/:id displays a character and all saved notes about them
-    it('can retrieve saved notes for a characterId', () => {
-      return request(app)
-        .post('/characters')
-        .send({ characterId: 100, note: 'This character rocks' })
-        .then(() => {
-          return request(app)
-            .get('/characters/100');
-        })
-        .then(res => {
-          expect(res.text).toContain('This character rocks');
-        });
-    });
-    it('can display error if no notes saved for a characterId', () => {
-      return request(app)
-        .get('/characters/1111')
-        .then(res => {
-          expect(res.text).toContain('No notes saved for character 1111');
-        });
-    });
-
-    // rick and morty character by ID
-    it('can return JSON that displays character details by ID', () => {
-      return request(app)
-        .get('/character/1')
-        .then(res => {
-          expect(res.body).toEqual({
-            name: 'Rick Sanchez',
-            species: 'Human',
-            status: 'Alive'
+      });
+  });
+  // POST /characters saves a note to the notes object
+  it('has a POST route and can save notes by characterId', done => {
+    return request(app)
+      .post('/characters')
+      .send({
+        characterId: 11, 
+        note: 'Rad character' 
+      })
+      .then(() => {
+        return request(app)
+          .post('/characters')
+          .send({
+            characterId: 11, 
+            note: 'another note about same character' 
           });
-        });
-    });
+      })
+      .then(() => {
+        return request(app)
+          .post('/characters')
+          .send({
+            characterId: 12, 
+            note: 'Different character note' 
+          });
+      })
+      .then(res => {
+        expect(res.status).toEqual(204);
+        done();
+      });
+  });
 
-    // POSTing data
-    it('has a POST route for adding notes', () => {
-      return request(app)
-        .post('/note')
-        .send({ text: 'Im a note' }) 
-        .then(res => {
-          expect(res.status).toEqual(204);
+  // GET /characters/:id displays a character and all saved notes about them
+  it('can retrieve saved notes for a characterId', () => {
+    return request(app)
+      .post('/characters')
+      .send({ characterId: 100, note: 'This character rocks' })
+      .then(() => {
+        return request(app)
+          .get('/characters/100');
+      })
+      .then(res => {
+        expect(res.text).toContain('This character rocks');
+      });
+  });
+  it('can display error if no notes saved for a characterId', () => {
+    return request(app)
+      .get('/characters/1111')
+      .then(res => {
+        expect(res.text).toContain('No notes saved for character 1111');
+      });
+  });
+
+  // rick and morty character by ID
+  it('can return JSON that displays character details by ID', () => {
+    return request(app)
+      .get('/character/1')
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'Rick Sanchez',
+          species: 'Human',
+          status: 'Alive'
         });
-    });
+      });
+  });
+
+  // POSTing data
+  it('has a POST route for adding notes', () => {
+    return request(app)
+      .post('/note')
+      .send({ text: 'Im a note' }) 
+      .then(res => {
+        expect(res.status).toEqual(204);
+      });
   });
 
   // Query Strings
@@ -126,6 +101,24 @@ describe('app', () => {
       .query({ name: 'cari' })
       .then(res => {
         expect(res.body).toEqual({ hi: 'there cari' });
+      });
+  });
+
+  // server and app (respond with text/html)
+  it('has a server and app testing route', () => {
+    return request(app)
+      .get('/tester-text')
+      .then(res => {
+        expect(res.text).toEqual('testing123');
+      });
+  });
+
+  // json app (respond with json)
+  it('has a json app testing route', () => {
+    return request(app)
+      .get('/tester-json')
+      .then(res => {
+        expect(res.body).toEqual({ testing: 123 });
       });
   });
 
