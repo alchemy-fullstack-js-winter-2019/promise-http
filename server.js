27dab0d49307ab1208e62c25473851cff59e26d9
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-29 12:47:53 -0800

    everything works except the note

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
 const http = require('http');
 const app = require('./lib/app');
-const parse = require('./lib/app');
 
-http.createServer(app, (req, res) => {
-  const url = parse(req.url);
-  if(url.pathname === '/tester') {
-    res.end('testing123');
-  }
-})
-  .listen(7890);
+http.createServer(app).listen(7890);
--- test/app.test.js
+++ test/app.test.js
@@ -21,7 +21,7 @@ describe('rick and morty api tests', () => {
       });
   });
 
-  it.only('gets notes for a character', () => {
+  it('gets notes for a character', () => {
     return request(app)
       .post('/characters')
       .send({ characterId: 1, note: 'Great character' })
@@ -34,34 +34,4 @@ describe('rick and morty api tests', () => {
       });
   });
 
-  // it('gets a character by id', () => {
-  //   return request(app)
-  //     .get('/character/1')
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         name: 'Rick Sanchez',
-  //         species: 'Human',
-  //         status: 'Alive'
-  //       });
-  //     });
-  // });
-
-  // it('can add a note to a character based on id', () => {
-  //   return request(app)
-  //     .post('/characters')
-  //     .send({ 1234: ['My favorite character'] })
-  //     .then(res => {
-  //       expect(res.status).toEqual(200);
-  //     });
-  // });
-
-  
-
-  // it('can get a character by id and their notes', () => {
-  //   return request(app)
-  //     .get('/characters/1')
-  //     .then(res => {
-  //       expect(res.status).toEqual(200);
-  //     });
-  // });
 });
